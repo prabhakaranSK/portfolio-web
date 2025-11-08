39f06e6361ab57698847117b0e8200fa5dfc2b9e
prabhakaranSK <prabhakaransv69@gmail.com>
2025-11-08 10:06:26 +0530

    done

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-   base: process.env.VITE_BASE_URL || '/portfolio-web/',
+  base: '/portfolio-web/', // Remove process.env for now
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
